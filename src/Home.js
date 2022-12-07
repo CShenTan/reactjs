461339501466e26f19108dbf77828f4bc5fd094b
CShenTan <fbajftwcst@gmail.com>
2022-12-08 04:25:44 +0800

    Update Home.js
    Test 1

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,46 @@
-import "./App.css";
-import { useState, useEffect } from "react";
+import "primeicons/primeicons.css"; //icons
+import { InputText } from "primereact/inputtext";
+import "primereact/resources/primereact.min.css"; //core css
+import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-
+import "./App.css";
 function Home() {
-  const [text, setText] = useState("");
+  const [name, setName] = useState("");
   const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    if (name === "Ready!") {
+      setIsReady(true);
+    } else {
+      setIsReady(false);
+    }
+  }, [name]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          hidden={!isReady}
-          src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
-          className="App-logo"
-          alt="logo"
-          style={{ padding: "10px" }}
-        />
-        <b>
-          Requirement: Try to show the hidden image and make it clickable that
-          goes to /pokedex when the input below is "Ready!" remember to hide the
-          red text away when "Ready!" is in the textbox.
-        </b>
+        <NavLink to="/pokedex">
+          <img
+            hidden={!isReady}
+            src="https://www.freeiconspng.com/uploads/file-pokeball-png-0.png"
+            className="App-logo"
+            alt="logo"
+            style={{ padding: "10px" }}
+          />
+        </NavLink>
         <p>Are you ready to be a pokemon master?</p>
-        <input type="text" name="name" />
-        <span style={{ color: "red" }}>I am not ready yet!</span>
+        <span className="p-float-label">
+          <InputText
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label>Type "Ready!"</label>
+        </span>
+        <span hidden={isReady} style={{ color: "red" }}>
+          I am not ready yet!
+        </span>
       </header>
     </div>
   );
